5288194640eed27d83e7e0af2476bae5ae5f1732
David P. Kleinschmidt <david@kleinschmidt.name>
2025-02-14 19:14:31 +0000

    It's all relative

--- static/facebox/facebox.js
+++ static/facebox/facebox.js
@@ -79,8 +79,8 @@
     settings: {
       opacity      : 0,
       overlay      : true,
-      loadingImage : '/static/facebox/loading.gif',
-      closeImage   : '/static/facebox/closelabel.gif',
+      loadingImage : 'loading.gif',
+      closeImage   : 'closelabel.gif',
       imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
       faceboxHtml  : '\
     <div id="facebox" style="display:none;"> \
@@ -97,7 +97,7 @@
                 </div> \
                 <div class="footer"> \
                   <a href="#" class="close"> \
-                    <img src="/static/facebox/closelabel.gif" title="close" class="close_image" /> \
+                    <img src="closelabel.gif" title="close" class="close_image" /> \
                   </a> \
                 </div> \
               </td> \
